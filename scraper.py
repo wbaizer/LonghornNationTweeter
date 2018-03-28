69df78929005b167583ab793f24e89bdbf1b24d8
wbaizer <will@thebaizers.com>
2018-03-28 09:06:10 -0700

    Updating process

--- scraper.py
+++ scraper.py
@@ -58,15 +58,15 @@ def already_scraped(post_id, created):
 			# If we've found a match then stop
 			## print line_copy + " == " + post_id + " -> " + str(line_copy == post_id)
 			if line_copy == post_id:
-				print "Match found for " + post_id
+				## print "Match found for " + post_id
 				return 1
 			# If we've gotten past SEARCH_BACK_DAYS days then stop
 			## print str(line_created) + " < " + str(post_date_check) + " -> " + str(float(line_created) < post_date_check)
 			if float(line_created) < post_date_check:
-				print "Looked past " + str(SEARCH_BACK_DAYS) + " days for " + post_id
+				## print "Looked past " + str(SEARCH_BACK_DAYS) + " days for " + post_id
 				return 0
 	# If there is no match then the current post hasn't been scraped.
-	print "No match found for " + post_id
+	## print "No match found for " + post_id
 	return 0
 
 def is_special_post(title, linkflair, author):
@@ -88,14 +88,12 @@ def scrape(reddit):
 	# put into list to reverse later
 	for submission in submissions:
 		sub_list.append(submission)
-	print sub_list
 	# Iterate through the subreddit's top X posts
 	# Reverse so newest posts are searched first
 	for submission in reversed(sub_list):
 		# Check to see if this submission has already been seen and scraped
 		been_scraped = already_scraped(submission.id, submission.created_utc)
-		print submission.id + " has been scraped? " + str(been_scraped)
-		
+		## print submission.id + " has been scraped? " + str(bool(been_scraped))
 		if not been_scraped:
 			# If post hasn't been scraped see if it meets minimum score requirements or is special
 			## print "Post is above 15? " + str(submission.score > MIN_SCORE) + " ||| Post is approved? "+ str(submission.approved) + " ||| And/Or post is special? " + str(is_special_post(submission.title, submission.link_flair_text, submission.author.name))
@@ -103,6 +101,7 @@ def scrape(reddit):
 				# Get an object with data for Twitter
 				# Append object to list to be exported to JSON
 				# Add post to scraped posts file
+				print "\rScraping post" + submission.id
 				submission_obj = create_submission_object(submission)
 				new_hot_posts.append(submission_obj)
 				with open(SCRAPED_POSTS_FILE, "a") as spf:
@@ -111,10 +110,11 @@ def scrape(reddit):
 	with open(REDDIT_OUTPUT, 'wb') as outfile:
 		json.dump(new_hot_posts, outfile)
 
-def main():
+def run():
 	
 	''' Driver for program '''
 	
+	print "Starting scraper..."
 	with open('credentials.json', 'r') as readfile:
 		credentials = json.load(readfile).get('reddit')
 	reddit = praw.Reddit(client_id=credentials.get('client_id'),
@@ -123,6 +123,8 @@ def main():
                          user_agent=credentials.get('user_agent'),
                          username=credentials.get('username'))
 	scrape(reddit)
+	print "Ending scraper..."
 	
 if __name__ == '__main__':
-	main()
+	print "Running scraper..."
+	run()
